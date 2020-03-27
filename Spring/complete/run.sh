aee32d2751b95d9dd3006841969471513a9ce2cb
David Gershtenkoren <dudi938@gmail.com>
2020-03-27 14:09:57 +0300

    change spring app

--- Spring/complete/run.sh
+++ Spring/complete/run.sh
@@ -5,4 +5,4 @@
 #./mvnw package && java -jar target/gs-spring-boot-docker-0.1.0.jar
 ./mvnw package && \ 
  docker build --no-cache  -t forcecout_erver . && \
- docker run --name  springboot -t -p 8080:8080 forcecout_erver
+ docker run --name  springboot -d -v log:/elk-with-filebeat-by-docker-compose/mylog -p 8080:8080 forcecout_erver
--- Spring/complete/src/main/java/hello/Application.java
+++ Spring/complete/src/main/java/hello/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 
-	String LogFilePath = "/Log.txt";
+	String LogFilePath = "/spring.log";
 
 	@RequestMapping("/")
 	public String index() {
