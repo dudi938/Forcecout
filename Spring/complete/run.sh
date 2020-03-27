94d83cc9645fd83837764e638e138c5aac4f2fc0
David Gershtenkoren <dudi938@gmail.com>
2020-03-27 15:15:54 +0300

    make exutablet the mvn script

--- Spring/complete/run.sh
+++ Spring/complete/run.sh
@@ -6,4 +6,4 @@
  chmod +x ./mvnw && \
 ./mvnw package && \ 
  docker build --no-cache  -t forcecout_erver . && \
- docker run --name  springboot -d -v log:/elk-with-filebeat-by-docker-compose/mylog -p 8080:8080 forcecout_erver
+ docker run --name  springboot -d -v /log:/elk-with-filebeat-by-docker-compose/mylog -p 8080:8080 forcecout_erver
